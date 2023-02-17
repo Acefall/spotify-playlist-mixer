d56c47b4e1e80ebcc5aeb4c8436e7fed30ada25d
Alexander Weinmann <me@aweinmann.de>
2023-02-17 19:43:30 +0100

    Dont use next source strategy anymore

--- spotify_playlist_mixer/testSpotifyPlaylist.py
+++ spotify_playlist_mixer/testSpotifyPlaylist.py
@@ -1,7 +1,8 @@
 from source.spotifyPlaylist import SpotifyPlaylist
 from source.takeN import TakeN
-from source.next_source_strategy.concatenate import Concatenate
-from source.combine import Combine
+from source.concatenate import Concatenate
+from source.repeatN import RepeatN
+from source.loop import Loop
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import json
@@ -16,24 +17,22 @@ sp = spotipy.Spotify(
                               redirect_uri="http://localhost:8080",
                               scope=scope))
 
-salsaPl = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/5Db6luvdhq3bEGwX3zcI5P?si=1e777cc298fe4fc4")
-bachataPl = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/4rPVgVb4xNOpexM22v5I72?si=8a4902de321a4f93")
-kizombaPl = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/0RPAReDJdaECIrco82WuhC?si=388dc32d20cc43b0")
-zoukPl = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/3BnuWDbMlEHzEnyC3zwS4q?si=9a52f63277124ee0")
+salsa = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/5Db6luvdhq3bEGwX3zcI5P?si=1e777cc298fe4fc4")
+bachata = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/4rPVgVb4xNOpexM22v5I72?si=8a4902de321a4f93")
+kizomba = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/0RPAReDJdaECIrco82WuhC?si=388dc32d20cc43b0")
+zouk = SpotifyPlaylist(sp, "https://open.spotify.com/playlist/3BnuWDbMlEHzEnyC3zwS4q?si=9a52f63277124ee0")
 
-salsaPattern = TakeN(3, salsaPl)
-bachataPattern = TakeN(2, salsaPl)
-kizombaPattern = TakeN(3, salsaPl)
-zoukPattern = TakeN(1, salsaPl)
+salsaPattern = TakeN(3, salsa)
+bachataPattern = TakeN(3, bachata)
+kizombaPattern = TakeN(3, kizomba)
+zoukPattern = TakeN(2, zouk)
 
-sbkSources = [salsaPattern, bachataPattern, kizombaPattern]
-concatenateSbk = Concatenate(sbkSources)
-combineSbk = Combine(sbkSources, concatenateSbk)
+sbk = Concatenate([salsaPattern, bachata, kizomba])
+sbk3 = RepeatN(3, sbk)
+sbkAndZouk = Concatenate([sbk3, zoukPattern])
 
-rootSources = [combineSbk, zoukPattern]
-rootConcatenate = Concatenate(rootSources)
 
-playlist = Combine(rootSources, rootConcatenate)
+playlist = Loop(sbkAndZouk)
 
 tracks = []
 for track in playlist:
@@ -42,7 +41,7 @@ for track in playlist:
 
 print(len(tracks))
 
-newPlaylist = sp.user_playlist_create("acefall", "testCombineTakeN")
+newPlaylist = sp.user_playlist_create("acefall", "complexPlaylist")
 for i in range(0, len(tracks), 100):
     sp.user_playlist_add_tracks(
         "acefall", newPlaylist["id"], tracks[i:i+100])
